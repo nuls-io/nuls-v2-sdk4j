7f87fa8feeae5c2624b92ee1b5923ba6e25555c6
MIMIEYES <pierreluohao@gmail.com>
2019-07-02 13:48:47 +0800

    jsonrpc util update

--- src/main/java/io/nuls/v2/util/JsonRpcUtil.java
+++ src/main/java/io/nuls/v2/util/JsonRpcUtil.java
@@ -3,25 +3,14 @@ package io.nuls.v2.util;
 import io.nuls.core.constant.CommonCodeConstanst;
 import io.nuls.core.log.Log;
 import io.nuls.core.parse.JSONUtils;
-import io.nuls.core.parse.MapUtils;
-import io.nuls.v2.model.dto.JsonRpcRequest;
 import io.nuls.v2.model.dto.RpcResult;
 import io.nuls.v2.model.dto.RpcResultError;
-import org.apache.commons.io.IOUtils;
-import org.apache.http.HttpEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
 
-import java.io.Closeable;
-import java.io.IOException;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-
-import static io.nuls.v2.util.RestFulUtil.connMgr;
-import static io.nuls.v2.util.RestFulUtil.requestConfig;
 
 /**
  * JSON-RPC 请求工具
@@ -36,16 +25,19 @@ public class JsonRpcUtil {
     private static final String JSONRPC = "jsonrpc";
     private static final String METHOD = "method";
     private static final String PARAMS = "params";
+    private static final String DEFAULT_ID = "1";
+    private static final String JSONRPC_VERSION = "2.0";
 
     public static RpcResult request(String method, List<Object> params) {
         RpcResult rpcResult;
         CloseableHttpClient httpClient = null;
         CloseableHttpResponse response = null;
         try {
-            JsonRpcRequest request = new JsonRpcRequest();
-            request.setMethod(method);
-            request.setParams(params);
-            Map<String, Object> map = MapUtils.beanToMap(request);
+            Map<String, Object> map = new HashMap<>(8);
+            map.put(ID, DEFAULT_ID);
+            map.put(JSONRPC, JSONRPC_VERSION);
+            map.put(METHOD, method);
+            map.put(PARAMS, params);
             String resultStr = HttpClientUtil.post(baseUrl, map);
             rpcResult = JSONUtils.json2pojo(resultStr, RpcResult.class);
         } catch (Exception e) {
