76bbade43cbc9d994c21da82b0c1777f1a9757ba
Charlie <29900478@qq.com>
2020-12-17 18:09:02 +0800

    update

--- src/test/java/io/nuls/v2/NaboxCollectionTest.java
+++ src/test/java/io/nuls/v2/NaboxCollectionTest.java
@@ -27,7 +27,6 @@ package io.nuls.v2;
 import io.nuls.core.basic.Result;
 import io.nuls.core.crypto.HexUtil;
 import io.nuls.core.exception.NulsException;
-import io.nuls.core.parse.I18nUtils;
 import io.nuls.core.parse.JSONUtils;
 import io.nuls.v2.error.AccountErrorCode;
 import io.nuls.v2.model.Account;
@@ -56,11 +55,6 @@ public class NaboxCollectionTest {
     @Before
     public void before() {
         //NulsSDKBootStrap.initTest("http://beta.api.nuls.io/");
-    }
-
-    @Test
-    public void test() throws NulsException {
-        I18nUtils.setLanguage("ee");
     }
 
     /**
